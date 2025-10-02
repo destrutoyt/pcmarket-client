13cdc9e3bd34b9654a4bef36b1d191beace1c7e4
Miguel Garces <miguelongriego24@gmail.com>
2025-10-02 00:18:44 -0400

    Updated "About Us" component, added animations, added two new components.

--- pc-market/src/app/about-us/about-us.component.ts
+++ pc-market/src/app/about-us/about-us.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-about-us',
@@ -6,6 +7,29 @@ import { Component } from '@angular/core';
   templateUrl: './about-us.html',
   styleUrl: './about-us.css'
 })
-export class AboutUsComponent {
+export class AboutUsComponent implements AfterViewInit {
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('is-visible');
+            observer.unobserve(entry.target);
+          }
+        });
+      }, {
+        threshold: 0.1
+      });
+
+      // Select all elements with the common class
+      const elementsToAnimate = document.querySelectorAll('.fade-on-scroll');
+
+      elementsToAnimate.forEach(element => {
+        observer.observe(element);
+      });
+    }
+  }
 }
---
+++ pc-market/src/app/seller-registration/seller-registration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-registration',
+  imports: [],
+  templateUrl: './seller-registration.component.html',
+  styleUrl: './seller-registration.component.css'
+})
+export class SellerRegistrationComponent {
+
+}
